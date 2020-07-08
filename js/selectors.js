6c2b5dca81569717db29cc969283b60d88d7a37d
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 08:40:59 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,7 +17,7 @@ function divSelector(){
 }
 
 function firstListItem(){}
-
+return $('ul#pic-list li:first-child')
 })
 
 Write a function paragraphSelector that does not accept any parameters. The function should use an element selector to select the p tags in index.html and return those elements. (Note that we're using the plural because the selector will always return an array when used with tags, even if there's only one!)
